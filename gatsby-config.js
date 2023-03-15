5be413fd864d7c5d0a1f6b230dd9f8f6310dd173
Mike Mathew <m2mathew@me.com>
2023-03-14 19:38:43 -0500

    initial commmmmmit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `New Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mike Mathew`,
+      summary: `is a human who writes code things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
